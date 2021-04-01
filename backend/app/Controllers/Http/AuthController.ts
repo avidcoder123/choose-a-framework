c0806f38c7ffe05989cb731b504ab549624d571c
avidcoder123 <umar.tli.edu@gmail.com>
2021-04-01 18:49:05 +0000

    create registration

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -1,4 +1,20 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import RegisterValidator from "App/Validators/RegisterValidator"
+import User from "App/Models/User"
 
 export default class AuthController {
+
+  public async register({ auth, request }: HttpContextContract) {
+    let user = await request.validate(RegisterValidator)
+
+    User.create({
+      username: user.username,
+      password: user.password
+    })
+
+    let token = await auth.use('api').attempt(user.username, user.password)
+
+    return token.toJSON()
+  }
+
 }
--- backend/app/Validators/RegisterValidator.ts
+++ backend/app/Validators/RegisterValidator.ts
@@ -1,4 +1,4 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class RegisterValidator {
@@ -25,6 +25,10 @@ export default class RegisterValidator {
 	 *    ```
 	 */
   public schema = schema.create({
+    username: schema.string({}, [
+      rules.unique({ table: 'users', column: 'username'})
+    ]),
+    password: schema.string()
   })
 
 	/**
@@ -38,5 +42,9 @@ export default class RegisterValidator {
 	 * }
 	 *
 	 */
-  public messages = {}
+  public messages = {
+    'username.required': "Username is required.",
+    'username.string': "Username must be string.",
+    'password.string': "Password must be string."
+  }
 }
