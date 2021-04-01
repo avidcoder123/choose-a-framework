cc1d5a47dff4b69f2169634df9f0a848a5d67d74
avidcoder123 <umar.tli.edu@gmail.com>
2021-04-01 19:54:28 +0000

    Task logic

--- backend/app/Controllers/Http/TasksController.ts
+++ backend/app/Controllers/Http/TasksController.ts
@@ -1,4 +1,48 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Task from 'App/Models/Task'
+import TaskValidator from 'App/Validators/TaskValidator'
 
 export default class TasksController {
+
+  public async list({ auth }: HttpContextContract) {
+
+    await auth.authenticate()
+
+    let tasks = await Task
+    .query()
+    .where("user_id", auth.user!.id)
+
+    return tasks.map((task) => {
+      task.toJSON()
+    })
+
+  }
+
+  public async create({ auth, request }: HttpContextContract) {
+
+    await auth.authenticate()
+
+    let task = await request.validate(TaskValidator)
+
+    Task.create({
+      'title': task.title,
+      'user_id': auth.user!.id
+    })
+
+    return "Success"
+
+  }
+
+  public async delete({ auth, request }: HttpContextContract) {
+
+    await auth.authenticate()
+
+    let task_id = request.input('task_id')
+
+    let task = await Task.findOrFail(task_id)
+
+    await task?.delete()
+
+    return "Success"
+  }
 }
